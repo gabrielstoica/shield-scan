7585710244b1582597c3efaf80d74845b1561161
Stoica Gabriel <marius_gabriel1998@yahoo.com>
2021-03-02 19:22:23 +0200

    Adaugare fisiere sursa

---
+++ shield-Scan.sh
@@ -0,0 +1,340 @@
+#! /usr/bin/bash
+set -e
+
+#SOURCES
+source google_safe_browsing.sh
+
+#DEFINE VARIABLES
+script_pid=$$
+fingerprints_uploads_dir="/home/kali/Desktop/fingerprints_uploads" #directory where SHA-256 hashes of files in uploads/ directory are stored
+uploads_dir="/home/kali/Desktop/uploads" #absolute path to uploads directory
+log_file="/home/kali/Desktop/log_file.txt" #log file where incidents are journalized
+config_dir_backup="/home/kali/Desktop/config_dir_backup"
+config_dir="/home/kali/Desktop/config_dir"
+integrity_file_location=$PWD"/"
+integrity_file="integrity_file.txt"
+raports_directory=$PWD"/raports"
+url_file="url_file.txt"
+
+#DEFINE COLORS AREA
+RED="tput setaf 1"
+WHITE="tput setaf 7"
+RED_BG="tput setab 1"
+GREEN_BG="tput setab 2"
+RESET="tput sgr 0"
+BOLD="tput bold"
+
+#DEFINE MESSAGES AREA
+WARNING="$($RED_BG)ATENTIE!$($RESET) $($WHITE)"
+CONFIRM="$($GREEN_BG)INTEGRITATE CONFIRMATA!$($RESET) $($WHITE)"
+NO_XSS="$($GREEN_BG)NU S-A DETECTAT CONTINUT XSS MALITIOS!$($RESET) $($WHITE)"
+
+function _scan_for_changes(){
+    
+    local FILE=$1
+    
+    local file_name="${FILE##$uploads_dir"/"}"
+    local file_hash=$(sha256sum $FILE | awk '{print $1}')
+    local trusted_hash=$(grep $file_name $integrity_file_location$integrity_file | awk '{print $3}' | head -1)
+    
+    # echo "Trusted hash of " $file_name " " $trusted_hash
+    # echo "Actual hash of " $file_name " " $file_hash
+    
+    #current_time=$(date +"%Y-%m-%d %T")
+    if [ -z $trusted_hash ]  #hash not found => new file
+    then
+        echo -e "new_file" $file_name
+    elif [ "$file_hash" != "$trusted_hash" ] #hash not equal with trusted one => file modifications
+    then
+        echo -e "exit_signal"
+    fi
+}
+
+###########################################################
+#Function that create fingerprints using SHA-256 algorithm#
+#of trusted files stored in a backup folder               #
+###########################################################
+function _compute_backup_integrity(){
+    if [ ! -f "$integrity_file_location$integrity_file" ]
+    then
+        touch $integrity_file_location$integrity_file
+    else
+        > $integrity_file_location$integrity_file
+    fi
+    
+    if [ -n "$1" ]
+    then
+        config_dir_backup=$1
+    fi
+    
+    for FILE in "$config_dir_backup/"*
+    do
+        if [ -f $FILE ]
+        then
+            local file_hash=$(sha256sum $FILE | awk '{ print $1 }')
+            local file_name="${FILE##$config_dir_backup"/"}"
+            echo -e $file_name" --> "$file_hash >> $integrity_file_location$integrity_file
+        else
+            echo "Fisier din config:" $FILE
+            for _FILE in "$FILE/"*
+            do
+                if [ -f $_FILE ]
+                then
+                    local file_hash=$(sha256sum $_FILE | awk '{ print $1 }')
+                    local file_name="${_FILE##$config_dir_backup"/"}"
+                    echo -e $file_name" --> "$file_hash >> $integrity_file_location$integrity_file
+                fi
+            done
+        fi
+    done
+}
+
+function _scan_uploads(){
+    if [ ! -f "$integrity_file_location$integrity_file" ]
+    then
+        touch $integrity_file_location$integrity_file
+    else
+        > $integrity_file_location$integrity_file
+    fi
+    
+    if [ ! -f "$log_file" ]
+    then
+        touch $log_file
+    fi
+    
+    check_result=0
+    _compute_backup_integrity $1
+    
+    while [ $check_result -eq 0 ]
+    do
+        for FILE in "$uploads_dir/"*
+        do
+            if [ -f "$FILE" ]
+            then
+                file_integrity=$(_scan_for_changes $FILE)
+                current_time=$(date +"%Y-%m-%d %T")
+                if [ "$file_integrity" == "exit_signal" ]
+                then
+                    echo "WARNING: FILE MODIFICATIONS OCCURED! CHECK LOG FILE!"
+                    echo -e "[ "$current_time" ] WARNING: FILE MODIFICATIONS in "$FILE": "${file_integrity//"new_file"/} >> $log_file
+                    exit 1
+                    check_result=1
+                elif echo $file_integrity | grep -q "new_file"
+                then
+                    echo "WARNING: NEW FILE UPLOADED! CHECK LOG FILE!"
+                    echo -e "[ "$current_time" ] WARNING: NEW FILE UPLOADED in "$uploads_dir": "${file_integrity//"new_file"/} >> $log_file
+                    exit 1
+                    check_result=1
+                else
+                    echo "scanning.."
+                    echo -e "[ "$current_time" ] NO PROBLEMES FOUND" >> $log_file
+                    check_result=0
+                fi
+            fi
+        done
+        sleep 1
+    done
+    
+}
+
+function _scan_integrity(){
+    _compute_backup_integrity
+    _check_integrity
+}
+
+function _create_url_file(){
+    local line=$1
+    local line_number=$2
+    
+    #echo $line
+    local check_for_url=$(echo $line | egrep -o "(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|\!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]")
+    
+    #send to Google Safe Browsing API
+    #to check if any URL from file
+    #is not on Green List
+    if [[ ! -z "$check_for_url" ]]
+    then
+        #echo "Linia $line_number, cu URL-ul: $check_for_url"
+        for url in $check_for_url
+        do
+            #echo $url
+            echo -e $line_number" --> " $url >> $url_file
+        done
+    fi
+}
+
+function _detect(){
+    local FILE=$1
+    local blacklist=("xss" "XSS" "onmouseover" "alert" "onerror" "document" "cookie" "document.cookie" "JaVaScRiPt" "iframe")
+    local line_number=1
+    if [ ! -f $url_file ]
+    then
+        touch $url_file
+    else
+        > $url_file
+    fi
+    
+    echo -e "Scanarea fisierului impotriva atacurilor de tip XSS a inceput..."
+    sleep 2
+    
+    local found=0
+    local total_warnings=0
+    while read line; do
+        #sleep 1
+        _create_url_file "$line" $line_number
+        
+        #loop through every line from file
+        #and check if any word is
+        #on XSS-attack-words list
+        for word in $line
+        do
+            found=0
+            for item in ${blacklist[@]}
+            do
+                
+                if [[ "$word" =~ $item ]] && [ $found -eq 0 ]
+                then
+                    echo -e "******************* $WARNING*******************"
+                    echo "A fost detectat continut potential malitios pe linia $line_number: cod JAVASCRIPT, potential atac de tip XSS"
+                    echo -e $line"\n"
+                    found=1
+                    total_warnings=$((total_warnings+1))
+                fi
+            done
+        done
+        line_number=$((line_number+1))
+    done < $FILE
+    
+    if [ $total_warnings -eq 0 ]
+    then
+        echo -e "*******************$NO_XSS*******************"
+        echo -e "Scanarea fisierului impotriva atacurilor de tip XSS a fost finalizata!"
+    else
+        echo -e "Scanarea fisierului impotriva atacurilor de tip XSS a fost finalizata!"
+    fi
+    
+    echo -e "\nScanning file against malicious URLs..."
+    _api_create_json "$url_file"
+    
+}
+
+function _compare_fingerprints(){
+    local FILE=$1
+    
+    local file_name="${FILE##$config_dir"/"}"
+    local file_hash=$(sha256sum $FILE | awk '{print $1}')
+    local trusted_hash=$(grep $file_name $integrity_file_location$integrity_file | awk '{print $3}' | head -1)
+    
+    echo "Trusted hash of " $file_name " " $trusted_hash
+    echo "Actual hash of " $file_name " " $file_hash
+    
+    current_time=$(date +"%Y-%m-%d %T")
+    if [ -z $trusted_hash ]  #hash not found => new file
+    then
+        local last_modification=$(date -r $FILE)
+        echo -e "$($BOLD)$WARNING Fisier nou adaugat: "$file_name
+        echo -e "Ultima modificare asupra fisierului a fost la data "$last_modification
+        echo -e "[ "$current_time" ] WARNING: FILE MODIFICATIONS in "$file_name". CHECK RAPORT FILE: "$raports_directory/$file_name"" >> $log_file
+        
+    elif [ "$file_hash" != "$trusted_hash" ] #hash not equal with trusted one => file modifications
+    then
+        
+        echo -e "$($BOLD)$WARNING Fisierul "$file_name" a fost modificat!"
+        echo -e "Verificati fisierul raport_"$file_name" pentru a investiga incidentul!"
+        echo -e "[ "$current_time" ] WARNING: FILE MODIFICATIONS in "$file_name". CHECK RAPORT FILE: "$raports_directory/$file_name"" >> $log_file
+        
+        _generate_raport $FILE $file_name
+    else
+        echo -e "$CONFIRM Integritatea fisierului "$file_name" confirmata!"
+    fi
+}
+
+function _generate_raport(){
+    
+    if [ ! -d $raports_directory ]
+    then
+        mkdir $raports_directory
+    else
+        rm -Rf $raports_directory/*
+    fi
+    #$1 compromised FILE
+    #$2 backup FILE
+    local FILE=$1
+    local file_name=$2
+    
+    local last_modification=$(date -r $FILE)
+    local modifications=$(sdiff $config_dir_backup"/"$file_name $FILE)
+    local raport_location=$(echo $raports_directory/"raport_"$file_name)
+    
+    touch $raport_location
+    echo -e "Fisierul a fost modificat la data "$last_modification >> $raport_location
+    echo -e "Urmatoarele modificari au avut loc:" >> $raport_location
+    echo $modifications >> $raport_location
+    #local generate_raport=$(sdiff $config_dir_backup"/"$file_name $FILE >> $raport_location)
+}
+
+function _check_integrity(){
+    for ENTRY in "$config_dir/"*
+    do
+        ###de modificat! doar fisierele de configurare sa fie
+        ###scanate
+        if [ -f $ENTRY ]
+        then
+            _compare_fingerprints $ENTRY
+        elif [ -d $ENTRY ]
+        then
+            for ENTRYS in "$ENTRY"/*
+            do
+                if [ -f $ENTRYS ]
+                then
+                    echo "Fisier din config_backup: " $ENTRYS
+                    _compare_fingerprints $ENTRYS
+                fi
+            done
+        fi
+    done
+}
+
+function _help(){
+    
+    logo_file="logo.txt"
+    
+    usage="\n shield-scan - Script created for scanning wordpress uploads/ folder and all wp-*.php files, \nhaving the mission to prevent potential RCE attacks and integrity of configuration files\n(c) Stoica Gabriel-Marius <marius_gabriel1998@yahoo.com> \n \nUsage: $(basename "$0") [-h] [-s n] \n \nwhere: \n \t -h Show help commands \n \t -s, -scan Scanning option: expect \"uploads\" or \"integrity\" \n"
+    
+    cat $logo_file
+    echo -e $usage
+}
+
+
+function main(){
+    if [ $# -eq 0 ]
+    then
+        _help
+    elif [ $1 == "--help" ] || [ $1 == "-h" ]
+    then
+        _help
+    elif [ $1 == "-scan" ] || [ $1 == "-s" ]
+    then
+        if [ $2 == "uploads" ]
+        then
+            _scan_uploads $uploads_dir
+        elif [ $2 == "integrity" ]
+        then
+            _scan_integrity
+        else
+            _help
+        fi
+    elif [ $1 == "-detect" ] || [ $1 == "-d" ]
+    then
+        if [ ! -f $2 ]
+        then
+            echo -e "Nu s-a putut realiza deschiderea fisierului $2!"
+        else
+            _detect $PWD"/"$2
+        fi
+    else
+        _help
+    fi
+}
+
+main "$@"
